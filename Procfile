b48832a1e38b7cc8447a114f666a2f741c6073a4
Thiago Muller <mullerthiago8@gmail.com>
2019-03-30 10:11:09 -0300

    testing

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: java -jar target/src/main/java/com/scilonax/scilobot/ScilobotApplication.java --p $PORT
+
---
+++ mit_news.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import datetime
+import logging
+
+logger = logging.getLogger()
+
+
+
+def mit_news_scraper():
+    #MIT news on machine learning
+    html = urlopen('https://news.mit.edu/topic/machine-learning')
+    soup = BeautifulSoup(html , 'html.parser')
+    article_cover_image = soup.findAll('div' , {'class' : 'article-cover-image'})
+    articles_partial_urls = [cover.find('a')['href'] for cover in article_cover_image]
+    articles_urls = [('https://news.mit.edu' + partial_url) for partial_url in articles_partial_urls]
+    valid_articles_with_correct_tags = []
+    current_date = datetime.datetime.today().strftime('%Y-%m-%d')
+
+    for article in articles_urls:
+        html = urlopen(article)
+        soup = BeautifulSoup(html , 'html.parser')
+        tags = soup.find('span' , {'class' : 'tags'}).findAll('a')
+        for tag in tags:
+            if(tag.get_text() == 'Machine learning'):
+                valid_articles_with_correct_tags.append(article)
+
+    for article in valid_articles_with_correct_tags:
+        html = urlopen(article)
+        soup = BeautifulSoup(html , 'html.parser')
+        if(soup.find('span' , {'itemprop' : 'datePublished'})['content'] == current_date):
+            return article
