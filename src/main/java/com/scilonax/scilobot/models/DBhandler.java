5c962162edd8cc84c4f4896e5fae5619afbdfd73
Thiago Muller <mullerthiago8@gmail.com>
2019-03-30 16:14:24 -0300

    forgot @repository annotation

--- src/main/java/com/scilonax/scilobot/models/DBhandler.java
+++ src/main/java/com/scilonax/scilobot/models/DBhandler.java
@@ -1,6 +1,7 @@
 package com.scilonax.scilobot.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
@@ -9,6 +10,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class DBhandler {
 
     @Autowired
